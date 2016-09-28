6a2ee6bc158ed9ad11c25f6d7bccb863e26b2b0c
Christian Lucero <crizlucero@gmail.com>
2016-09-28 18:06:25 -0500

    Creación de base de datos en couchDB

---
+++ Mosyn/MosynExample/Classess/database.py
@@ -0,0 +1,20 @@
+import couchdb
+
+class Database:
+    conn = None
+    db = None
+
+    def __init__(self):
+        self.conn =couchdb.Server("http://example.com:5984/")
+
+    def createDB(self):
+        self.db = self.conn.create("EsParser")
+
+    def deleteDB(self):
+        self.conn.delete("EsParser")
+
+    def saveData(self, document):
+        self.db.save(document)
+
+    def getData(self):
+        return None
